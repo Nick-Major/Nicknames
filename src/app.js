d3399e457867c022b224ba0a3c197ae211a6126e
Nick-Major <divanchik26@gmail.com>
2024-11-01 12:00:06 +0300

    Test added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-let user = '-Player'
+export let user = '-Player'
 
-function validateUsername(userName) {
+export function validateUsername(userName) {
     let validCharacters = /[(a-z\_\-0-9)]/.test(userName);
     let startsWithNumbersAndSymbols = /^[^\_\-0-9]/.test(userName);
     let endsWithNumbersAndSymbols = /[^\_\-0-9]$/.test(userName);
---
+++ test/test.js
@@ -0,0 +1,5 @@
+import { user, validateUsername } from "../src/app.js";
+
+test ('validateUsername test', () => {
+    
+})
